252ec89496129515a512e25c0c2fc4a849d7e724
faradilahrahman12-art <faradilahrahman12@gmail.com>
2026-09-26 18:40:05 +0800

    Update robot.ts

--- app/robot.ts
+++ app/robot.ts
@@ -7,6 +7,5 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
     },
     sitemap: "https://www.bunnyticket.store/sitemap.xml",
-    host: "https://www.bunnyticket.store",
   };
 }
